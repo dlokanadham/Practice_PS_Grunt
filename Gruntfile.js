a8d149f04929e31930fe34ad0b680c8ea1f0a584
dasamukl <lokanadham.dasamukha@cdk.com>
2016-10-11 13:06:22 +0530

    added grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -42,6 +42,68 @@ module.exports = function(grunt){
                //eautify: true,
             },
         }, 
+		htmlhint:{
+			templates:{
+				options:{
+					'attr-lower-case':true,
+					'attr-value-not-empty':true,
+					'tag-pair':true,
+					'tag-self-close':true,
+					'tagname-lowercase':true,
+					'id-class-value':true,
+					'id-class-unique':true,
+					'src-not-empty':true,
+					'img-alt-required':true
+				},
+				src:['templates/*.html']
+			}
+		},
+
+		htmlmin:{
+			dev:{
+				options:{
+				removeEmptyAttributes:true,
+				removeEmptyElements : true,
+				removeRedundantAttributes : true,
+				removeComments : true,
+				removeOptionslTags :true
+			},
+			files:{
+				'templates/bundle.min.html':['templates/first.html', 'templates/second.html']
+			}
+			}
+			
+		},
+		less:{	
+		development:{
+			options:{
+				cleancss:true,
+				compresscss:false
+			},
+			files:[
+				{
+					expand:true,
+					cwd:'Content/',
+					src:'less/*.less',
+					dest:'css/',
+					ext:'.css',
+					extDot:'last'
+				}
+			]
+		}},
+		csslint:{
+			strict:{
+				options:{},
+				src:['Content/css/*.css']
+			},
+			laxed:{
+				options:{
+					csslintrc:'csslintrules.json'
+					
+				},
+				src:['Content/css/*.css']
+			}
+		}
 
 	});
 
@@ -50,11 +112,18 @@ module.exports = function(grunt){
 	grunt.loadNpmTasks('grunt-typescript');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
-
+	grunt.loadNpmTasks('grunt-htmlhint');
+	grunt.loadNpmTasks('grunt-contrib-htmlmin');
+	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-csslint');
 
 	grunt.registerTask('default','clean');
-	grunt.registerTask('tsToJs','typescript');
-	grunt.registerTask('analyze','jshint');
-	grunt.registerTask('compress','uglify');
-
+		grunt.registerTask('tsToJs','typescript');
+		grunt.registerTask('analyze','jshint');
+		grunt.registerTask('compress','uglify');
+		grunt.registerTask('htmlanalyze','htmlhint');
+	 grunt.registerTask('htmlminification','htmlmin');
+	 grunt.registerTask('lesstocss','less');
+	 grunt.registerTask('cssverify', 'csslint');
+	 grunt.registerTask('cssminify', 'cssmin');
 };
